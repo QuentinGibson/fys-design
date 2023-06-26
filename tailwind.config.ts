655c26a188a3f6b251dfd4c0c965c291df376f54
Quentin Gibson <quentingibson94@gmail.com>
2023-06-26 17:01:33 -0400

    add variables for colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ export default {
   darkMode: "class",
   theme: {
     extend: {},
+    colors: {
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+      secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+    },
   },
   plugins: [],
 } satisfies Config;
