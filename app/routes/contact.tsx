755caf68194b92ec1d15fdcb1460de256431f7e8
Quentin Gibson <qgibson3@students.kennesaw.edu>
2023-09-09 23:28:57 -0400

    add contact page

---
+++ app/routes/contact.tsx
@@ -0,0 +1,13 @@
+import ContactForm from "~/components/ContactForm";
+import Squiggle from "~/components/Squiggle";
+
+export default function ContactRoute() {
+  return (
+    <main>
+      <div className="py-8">
+        <Squiggle />
+        <ContactForm />
+      </div>
+    </main>
+  );
+};
